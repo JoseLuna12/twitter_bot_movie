77541927bae1694cddbb1cd2d8f811ab38049dc6
joseluna12 <pepejose@outlook.com>
2022-11-25 19:20:05 -0500

    better ai

--- utils/openAi.js
+++ utils/openAi.js
@@ -11,8 +11,8 @@ async function resumeMovie(text) {
     if (text) {
         const completion = await openai.createCompletion({
             model: "text-davinci-002",
-            prompt: `Sumarize this Parragraph in third person on one sentence.
-            Parragraph: ${text}
+            prompt: `Sumarize this paragraph in one sentence.
+            paragraph: ${text}
             sentence:`,
             temperature: 0,
             max_tokens: 60,
